b75a7890c5990af471dec1333fbe9f6f2883dfff
Sahil Sonal <contact.sahilsonal@gmail.com>
2025-09-24 02:25:08 +0530

    Use Vercel analytics script fallback

--- khel-saarthi/src/main.jsx
+++ khel-saarthi/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";   // <- Tailwind yahin se load hota hai
-import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
