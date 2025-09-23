377fb8c8eaddd9baae0d91295dc543894c7efa18
Sahil Sonal <contact.sahilsonal@gmail.com>
2025-09-24 02:18:33 +0530

    Add Vercel Analytics

--- khel-saarthi/src/main.jsx
+++ khel-saarthi/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";   // <- Tailwind yahin se load hota hai
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
